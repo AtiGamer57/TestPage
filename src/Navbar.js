4452e05266e79cd1479f97c56461991ef6a7287e
Futó Attila <futo.attila02@gmail.com>
2024-02-28 16:53:49 +0100

    Replace placeholder text in navbar and add partial css

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,20 +2,24 @@ import React from "react";
 
 function Navbar() {
     return (
-        <nav className="flex justify-between items-center my-4 font-Poppins">
-            <div id="logo">Your Logo</div>
-            <div id="middle-pages">
-                <a href="#">Page 1</a>
-                <a href="#">Page 2</a>
-                <a href="#">Page 3</a>
-                <a href="#">Page 4</a>
-            </div>
-            <div id="right-pages">
-                <a href="#">Page 5</a>
-                <a href="#">Page 6</a>
-                <a href="#">Page 7</a>
-            </div>
-        </nav>
+        <>
+            <nav className="flex justify-between items-center font-Poppins leading-6 my-8 text-gray-400">
+                <div id="logo" className="font-bold w-auto text-lg text-black">Vence<p className="inline text-red-300">.</p></div>
+                <div id="middle-pages" className="mx-4 ">
+                    <a href="#" className="mx-2 w-auto font-normal text-sm">Example</a>
+                    <a href="#" className="mx-2 w-auto font-normal text-sm">Inspiration</a>
+                    <a href="#" className="mx-2 w-auto font-normal text-sm">Tutorials</a>
+                    <a href="#" className="mx-2 w-auto font-normal text-sm">Courses</a>
+                </div>
+                <div id="right-pages" className="w-auto flex">
+                    <a href="#" className="mx-4 w-auto font-normal text-sm">Fb</a>
+                    <a href="#" className="mx-4 w-auto font-normal text-sm">Tw</a>
+                    <a href="#" className="mx-4 w-auto font-normal text-sm">Ln</a>
+                    <div className="mx-3 border-l-2 border-gray-300"></div>
+                    <p className="ml-4 w-auto font-normal text-sm">&#x1F50E;&#xFE0E;</p>
+                </div>
+            </nav>
+        </>
     );
 }
 
