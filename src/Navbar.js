1fb186dc2d75f97d07b66a8ed91d321f6075f3e7
Futó Attila <futo.attila02@gmail.com>
2024-02-28 17:14:58 +0100

    Add hover animation to navbar elements

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,10 +6,10 @@ function Navbar() {
             <nav className="flex justify-between items-center font-Poppins leading-6 my-8 text-gray-400">
                 <div id="logo" className="font-bold w-auto text-lg text-black">Vence<p className="inline text-red-300">.</p></div>
                 <div id="middle-pages" className="mx-4 ">
-                    <a href="#" className="mx-2 w-auto font-normal text-sm">Example</a>
-                    <a href="#" className="mx-2 w-auto font-normal text-sm">Inspiration</a>
-                    <a href="#" className="mx-2 w-auto font-normal text-sm">Tutorials</a>
-                    <a href="#" className="mx-2 w-auto font-normal text-sm">Courses</a>
+                    <a href="#" className="mx-2 w-auto font-normal text-sm underline decoration-[3px] under transition-all duration-300 ease-in-out underline-offset-8 decoration-red-200/0 hover:decoration-red-200">Example</a>
+                    <a href="#" className="mx-2 w-auto font-normal text-sm underline decoration-[3px] under transition-all duration-300 ease-in-out underline-offset-8 decoration-red-200/0 hover:decoration-red-200">Inspiration</a>
+                    <a href="#" className="mx-2 w-auto font-normal text-sm underline decoration-[3px] under transition-all duration-300 ease-in-out underline-offset-8 decoration-red-200/0 hover:decoration-red-200">Tutorials</a>
+                    <a href="#" className="mx-2 w-auto font-normal text-sm underline decoration-[3px] under transition-all duration-300 ease-in-out underline-offset-8 decoration-red-200/0 hover:decoration-red-200">Courses</a>
                 </div>
                 <div id="right-pages" className="w-auto flex">
                     <a href="#" className="mx-4 w-auto font-normal text-sm">Fb</a>
