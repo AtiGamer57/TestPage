d7ef4ac8cd026e3abc313179708944f0c115152b
Futó Attila <futo.attila02@gmail.com>
2024-03-01 16:25:21 +0100

    Add background images to trending grid

--- src/Body.js
+++ src/Body.js
@@ -3,13 +3,13 @@ import React from "react";
 function Body() {
     return (
         <>
-        <h1 className=" font-bold text-3xl">Trending</h1>
-        <div className=" grid grid-cols-3 gap-2 w-full">
-            <div className="col-span-2">Placeholder</div>
-            <div className="">Placeholder</div>
-            <div className="">Placeholder</div>
-            <div className="">Placeholder</div>
-            <div className="">Placeholder</div>
+        <h1 className=" font-bold text-3xl my-3">Trending</h1>
+        <div className=" h-svh grid grid-cols-3 gap-8 w-full mb-10">
+            <div className=" h-full h-96 bg-[url('assets/images/trending1.jpg')] bg-center rounded-xl bg-cover col-span-2">Placeholder</div>
+            <div className=" h-full h-96 bg-[url('assets/images/trending2.jpg')] bg-center rounded-xl bg-cover ">Placeholder</div>
+            <div className=" h-full h-96 bg-[url('assets/images/trending3.jpg')] bg-center rounded-xl bg-cover ">Placeholder</div>
+            <div className=" h-full h-96 bg-[url('assets/images/trending4.jpg')] bg-center rounded-xl bg-cover ">Placeholder</div>
+            <div className=" h-full h-96 bg-[url('assets/images/trending5.jpg')] bg-center rounded-xl bg-cover ">Placeholder</div>
         </div>
         </>
     );
