498c06b16bb9ae4d7b60b6a45c7c7dd169376801
Futó Attila <futo.attila02@gmail.com>
2024-03-01 22:25:18 +0100

    Add style to api called images

--- src/ApiImages.js
+++ src/ApiImages.js
@@ -4,13 +4,11 @@ const ApiImages = () => {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const [asd, setAsd] = useState(1);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await fetch('https://picsum.photos/v2/list?page=2&limit=9');
-                setAsd(2);
                 console.log(response.ok);
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
@@ -18,6 +16,7 @@ const ApiImages = () => {
                 const result = await response.json();
                 setData(result);
             } catch (error) {
+                console.log(error);
                 setError(error);
             } finally {
                 setLoading(false);
@@ -28,15 +27,14 @@ const ApiImages = () => {
     }, []); // Empty dependency array means the effect runs once after the initial render
 
     return (
-        <div className=''>
-            <h1>Images That you should randomly see</h1>
+        <div className='font-bold text-3xl mt-3 mb-10 relative'>
+            <h1 className='my-8'>Images That you should randomly see</h1>
             {loading && <p>Loading...</p>}
-            {error && <p>Error: {error.message}</p>}
             {data.length > 0 && (
-                <div className=' h-svh grid grid-cols-3 w-full mb-10'>
+                <div className=' grid grid-cols-3 w-full gap-5 mb-10'>
                     {data.map((item) => (
-                        <div>
-                            <img src={item.download_url} alt='random image'/>
+                        <div className=' overflow-hidden rounded-3xl relative'>
+                            <img className='object-contain w-full rounded-3xl left-1/2  my-auto' src={item.download_url} alt='random image'/>
                         </div>
                     ))}
                 </div>
