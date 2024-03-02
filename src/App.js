b99cb54ff8660d9c7608b288fda38e168184b7ed
Futó Attila <futo.attila02@gmail.com>
2024-03-02 15:35:10 +0100

    Add greeting component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Navbar from './Navbar';
 import Trending from './Trending';
 import ApiImages from './ApiImages';
+import Greeting from './Greeting';
 
 function App() {
   return (
     <div className=' w-10/12 absolute left-1/2 -translate-x-1/2 font-Poppins'>
       <Navbar />
+      <Greeting />
       <ApiImages />
       <Trending />
       <div className="App">
